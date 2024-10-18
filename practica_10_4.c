a02ff7f0a56578bbeb83aedb8bb96c6ebb218696
RodolfoAragom5 <143585799+RodolfoAragom5@users.noreply.github.com>
2024-10-18 14:39:29 -0600

    Update practica_10_4.c

--- practica_10_4.c
+++ practica_10_4.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
   int main ()
   {
-  int arr[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}; // Se declara e inicializa el arreglo
+  int arr[3][3]; // Se declara e inicializa el arreglo
   int x = 0;
   int y=0;
-  printf("arreglo\n");
+    
+  for (x=0; x<3; x++) 
+  {
+    printf("\n");
+    
+    for (y=0; y<3; y++)
+      {
+        printf("ingresa el valor del arreglo en la columna %d en la fila %d \n", x, y);
+        scanf("%d", &arr[x][y]);
+      }
+  }
+    printf("arreglo\n");
   for (x=0; x<3; x++) 
   {
     printf("\n");
