803fd3608fd55767aad35f53abae6273994c95b4
RodolfoAragom5 <143585799+RodolfoAragom5@users.noreply.github.com>
2024-10-18 14:19:52 -0600

    Create practica_10_4.c

---
+++ practica_10_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  int main ()
+  {
+  int arr[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}; // Se declara e inicializa el arreglo
+  int x = 0;
+  int y=0;
+  printf("arreglo\n");
+  for (x=0; x<3; x++) 
+  {
+    printf("\n");
+    for (y=0; y<3; y++)
+      {
+        printf("\t %d", arr[x][y]);
+      }
+  }
+  return 0;
+  }
